7fb6a656503097e042ee7d813941262b77c6d9da
Pablo <pablito@me.com>
2023-09-23 18:31:08 +0100

    Basic app setup with simple backend server

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 5555;
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
