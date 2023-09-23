8d069c03eea41868c2298d4421d520366ec57630
Pablo <pablito@me.com>
2023-09-23 22:30:37 +0100

    Added health check endpoint for loading test - test works well on backend server only test

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 5555;
+const port = 5501;
 const cors = require('cors');
 
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/health-check', (req, res) => res.send('OK'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
